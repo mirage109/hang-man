71fad35acf69260bb467bf6e3be478df9c844f91
Anastasiia Zibla <zibla.asia@gmail.com>
2024-01-29 20:57:49 +0100

    feat; styles  end

--- src/components/HangmanWord/index.tsx
+++ src/components/HangmanWord/index.tsx
@@ -1,34 +1,21 @@
+import styles from './style.module.css';
+
 type HangmanWordProps = {
   guessedLetters: string[];
   wordToGuess: string;
   revial?: boolean;
 };
+
 export const HangmanWord = ({
   guessedLetters,
   wordToGuess,
   revial = false,
 }: HangmanWordProps) => {
   return (
-    <div
-      style={{
-        display: "flex",
-        gap: ".25em",
-        fontSize: "6rem",
-        fontWeight: "bold",
-        textTransform: "uppercase",
-        fontFamily: "monospace",
-      }}
-    >
+    <div className={styles.hangmanWordContainer}>
       {wordToGuess.split("").map((letter, index) => {
         return (
-          <div
-            key={index}
-            style={{
-              borderBottom: ".1em solid black",
-              width: "1em",
-              textAlign: "center",
-            }}
-          >
+          <div key={index} className={styles.hangmanLetterContainer}>
             <span
               style={{
                 visibility:
