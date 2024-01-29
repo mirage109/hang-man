142c1c9a9be2acd9206f8ce776697f7fe09d758c
Anastasiia Zibla <zibla.asia@gmail.com>
2024-01-29 20:49:52 +0100

    feat: module styles

---
+++ src/components/HangmanDrawing/bodyParts.tsx
@@ -0,0 +1,8 @@
+import styles from "./style.module.css";
+
+export const Head = <div className={styles.head} />;
+export const Body = <div className={styles.body} />;
+export const RightArm = <div className={styles.rightArm} />;
+export const LeftArm = <div className={styles.leftArm} />;
+export const RightLeg = <div className={styles.rightLeg} />;
+export const LeftLeg = <div className={styles.leftLeg} />;
--- src/components/HangmanDrawing/index.tsx
+++ src/components/HangmanDrawing/index.tsx
@@ -1,125 +1,18 @@
-const Head = (
-  <div
-    style={{
-      width: "50px",
-      height: "50px",
-      borderRadius: "100%",
-      border: "10px solid black",
-      position: "absolute",
-      top: "50px",
-      right: "-30px",
-    }}
-  />
-);
-
-const Body = (
-  <div
-    style={{
-      width: "10px",
-      height: "100px",
-      background: "black",
-      position: "absolute",
-      top: "120px",
-      right: 0,
-    }}
-  />
-);
-
-const RightArm = (
-  <div
-    style={{
-      width: "100px",
-      height: "10px",
-      background: "black",
-      position: "absolute",
-      top: "150px",
-      right: "-100px",
-      rotate: "-30deg",
-      transformOrigin: "left bottom",
-    }}
-  />
-);
-const LeftArm = (
-  <div
-    style={{
-      width: "100px",
-      height: "10px",
-      background: "black",
-      position: "absolute",
-      top: "150px",
-      right: "10px",
-      rotate: "30deg",
-      transformOrigin: "right bottom",
-    }}
-  />
-);
-
-const RightLeg = (
-  <div
-    style={{
-      width: "100px",
-      height: "10px",
-      background: "black",
-      position: "absolute",
-      top: "210px",
-      right: "-90px",
-      rotate: "60deg",
-      transformOrigin: "left bottom",
-    }}
-  />
-);
-
-const LeftLeg = (
-  <div
-    style={{
-      width: "100px",
-      height: "10px",
-      background: "black",
-      position: "absolute",
-      top: "210px",
-      right: 0,
-      rotate: "-60deg",
-      transformOrigin: "right bottom",
-    }}
-  />
-);
-
+import { Head, Body, RightArm, LeftArm, RightLeg, LeftLeg } from "./bodyParts";
+import styles from "./style.module.css";
 const BodyParts = [Head, Body, RightArm, LeftArm, RightLeg, LeftLeg];
 
 type HangmanDrawingProps = {
   numberOfGuesses: number;
 };
-export const HangmanDrawing = ({numberOfGuesses}: HangmanDrawingProps) => {
+export const HangmanDrawing = ({ numberOfGuesses }: HangmanDrawingProps) => {
   return (
     <div style={{ position: "relative" }}>
       {BodyParts.slice(0, numberOfGuesses)}
-      <div
-        style={{
-          height: "50px",
-          width: "10px",
-          background: "black",
-          position: "absolute",
-          top: "0",
-          right: "0",
-        }}
-      />
-      <div
-        style={{
-          height: "10px",
-          width: "200px",
-          background: "black",
-          marginLeft: "120px",
-        }}
-      />
-      <div
-        style={{
-          height: "400px",
-          width: "10px",
-          background: "black",
-          marginLeft: "120px",
-        }}
-      />
-      <div style={{ height: "10px", width: "250px", background: "black" }} />
+      <div className={styles.rope} />
+      <div className={styles.top}/>
+      <div className={styles.pole} />
+      <div className={styles.base} />
     </div>
   );
 };
